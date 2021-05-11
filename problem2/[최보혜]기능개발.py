3d8cfefbb9fa3ef447075295149f595d4815005a
BoHye0202 <55703579+BoHye0202@users.noreply.github.com>
2021-05-11 22:56:58 +0900

    Update [최보혜]기능개발.py

--- problem2/[최보혜]기능개발.py
+++ problem2/[최보혜]기능개발.py
@@ -1,25 +1,28 @@
 def solution(progresses, speeds):
+    
+    # 걸리는 날짜를 계산한 Q 리스트 만들기
     Q = []
     for i,j in zip(progresses, speeds):
         x = (100 - i) // j
-        if (100-j)%j!=0:
+        if (100-j)%j!=0: # 값이 나누어 떨어지지 않았다면 1 더하기
             x+=1
-        Q.append(x)
-    result, i = [], 0
-    cnt = 1
+        Q.append(x) # Q리스트에 추가
     
-    check = Q[i]
+    # result 구하기
+    result, i = [], 0 # result: 결과 담을 리스트, i: 인덱스
+    cnt = 1 # count
+    check = Q[i] # 기준점이 되는 값 (비교할 값)
     while True:
-        if i+1 == len(Q):
+        if i+1 == len(Q): # 마지막 값이면 cnt 추가하고 while문 종료
             result.append(cnt)
             break
-        if check<Q[i+1]:
+        if check<Q[i+1]: # 기준값보다 값이 크면 cnt추가
             result.append(cnt)
-            cnt = 1
-            check = Q[i+1]
-        else:
+            cnt = 1 # cnt값 원래대로
+            check = Q[i+1] # 기준값은 그 다음 인덱스값으로 바꿔주기
+        else: # 기준값보다 작으면 cnt 더하기
             cnt+=1
-        i+=1
+        i+=1 # 인덱스 더하기
     return result
 
 solution([93, 30, 55], 	[1, 30, 5]) # [2, 1]
