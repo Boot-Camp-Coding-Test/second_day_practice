827bcfb58e48a42f75be04dd75db052f4f742df0
BoHye0202 <55703579+BoHye0202@users.noreply.github.com>
2021-05-11 21:17:44 +0900

    Update [최보혜]완주하지 못한 선수.py

--- problem1/[최보혜]완주하지 못한 선수.py
+++ problem1/[최보혜]완주하지 못한 선수.py
@@ -1,6 +1,7 @@
 from collections import Counter
+
 def solution(participant, completion):
-    participant = Counter(participant)
-    completion = Counter(completion)
-    answer = participant-completion
-    return list(answer.keys())[0]
+    participant = Counter(participant) # participant dict형태 count
+    completion = Counter(completion) # completion dict형태 count
+    answer = participant-completion # 빼서 하나 남기기
+    return list(answer.keys())[0] # 그냥 anwer[0]은 values값 나오기 때문에 key를 리스트로 바꿔서 출력
